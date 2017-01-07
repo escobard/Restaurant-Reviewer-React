e653a5d87d8e5eb18a70e343437cdda53389657f
Daniel Escobar <escobard90@gmail.com>
2017-01-06 20:22:21 -0700

    type: feat
    re-organized App DOM layout

--- src/components/app.js
+++ src/components/app.js
@@ -12,8 +12,8 @@ export default class App extends Component {
   render() {
     return (
       <div>
-		<RestaurantList />
       	<RestaurantDetail />
+      	<RestaurantList />
       </div>
     );
   };
