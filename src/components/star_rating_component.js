e306aa91005192efef591481aca7c2afed95b463
Daniel Escobar <escobard90@gmail.com>
2017-01-19 22:34:23 -0700

    type: feat
    created basic star component

--- src/components/star_rating_component.js
+++ src/components/star_rating_component.js
@@ -3,3 +3,27 @@
 // 	star_rating_component.js
 //
 // =============================================================
+
+// this was created to add non-editable star component for each restaurant list / detail
+
+// dependacies
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import StarRatingComponent from 'react-star-rating-component';
+
+export default class starRating extends Component {
+    render() {
+        const { rating } = this.state;
+        return (                
+            <div>
+                <h2>Rating from state: {rating}</h2>
+                <StarRatingComponent 
+                    name="starRating" 
+                    editing={false}
+                    starCount={5}
+                    value={props.value}
+                />
+            </div>
+        );
+    }
+}
