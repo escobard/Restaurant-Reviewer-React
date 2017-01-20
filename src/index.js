9f433020f3c8e039e55e4983d0601d4a086c72f8
Daniel Escobar <escobard90@gmail.com>
2017-01-20 00:18:16 -0700

    type: feat
    form working, will need to create a reducer to store data

--- src/index.js
+++ src/index.js
@@ -14,3 +14,26 @@ ReactDOM.render(
   </Provider>
   , document.querySelector('.application'));
 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+
+// We'll create this in step 3.
+import store from './store.js';
+
+// We'll create this in step 4.
+import UserForm from './components/UserForm.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Provider store={ store }>
+        <UserForm />
+      </Provider>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('.application'));
---
+++ src/store.js
@@ -0,0 +1,17 @@
+// ./store.js
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+import { combineForms } from 'react-redux-form';
+
+const initialUserState = {
+  firstName: '',
+  lastName: ''
+};
+
+const store = createStore(combineForms({
+  user: initialUserState,
+}));
+
+export default store;
