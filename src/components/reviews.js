69e28d9c5a08f35e92a31c1d589aef5535b376b6
Daniel Escobar <escobard90@gmail.com>
2017-01-22 15:24:50 -0700

    type: refactor
    refactored reviews into their own component

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -6,9 +6,41 @@
 
 import React, { Component } from 'react';
 
-export default class Nav extends Component {
+export default class Reviews extends Component {
+  	// adds review image based on user gender
+	renderReviewImage(reviewImage){
+		    if (reviewImage === "male"){
+		        return (
+		        	<img src="../../img/user-male.png" alt="male profile picture"/>
+		        );
+		    }
+		    else if (reviewImage === "female"){
+		    	return (
+		        <img src="../../img/user-female.png" alt="female profile picture"/>
+		        );
+		    }
+			else {
+		        return (
+		        	<img src="../../img/user-male.png" alt="male profile picture"/>
+		        );
+		    }
+	}	
+
   render() {
     return (
+    	<div className="restaurantReview col-md-12" key={reviews.name}>
+					<div className="userDetails col-md-4">
+						{this.renderReviewImage(reviews.image)}
+						<span>{reviews.name}</span>
+						<span>{reviews.date}</span>
+
+					</div>
+					<div className="reviewDetails col-md-8">
+						<div className="starContainer col-md-12"><StarRatingWidget rating={rating} /></div>	
+						<span className="col-md-12">"{reviews.comments}"</span>
+					</div>
+					
+		</div>
     );
   }
 }
