afaa6cc0ae5e94575c19d5f0558afb3d4f3c8932
Daniel Escobar <escobard90@gmail.com>
2017-01-23 16:20:13 -0700

    type: feat
    added style handlers for returning to app home

--- src/containers/restaurant_detail.js
+++ src/containers/restaurant_detail.js
@@ -29,7 +29,8 @@ class RestaurantDetail extends Component {
 
 		var restaurantDetails = document.querySelector('.restaurantDetails');
 		var restaurantList = document.querySelector('.restaurantList');
-
+		var postReview = document.querySelector('#postReview');
+		var reviewPost = document.querySelector('#reviewPost');
 		restaurantDetails.classList.remove('fadeInDown');
 		restaurantDetails.classList.add('fadeOut');
 		restaurantDetails.classList.add('heightHidden');
@@ -37,11 +38,18 @@ class RestaurantDetail extends Component {
 			restaurantDetails.classList.add('hidden');
 		}, 1000);
 		
-		// handles classes for the restaurant list
+		// handles views for the restaurant list
 		restaurantList.classList.remove('fadeOutDown');
 		restaurantList.classList.remove('hidden');
 		restaurantList.classList.add('fadeInUp');
+		
+		// handles views for the postReview
+		postReview.classList.add('hidden');
+		postReview.classList.remove('fadeInUp');
 
+		// handles views for the reviewPost
+		reviewPost.classList.add('hidden');
+		reviewPost.classList.remove('fadeInUp');
 	}
 
 	renderComments(){
